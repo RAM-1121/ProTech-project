1a704c56f201b1a756f0013935df9aa2e3522781
SouMyth <soumyth@soumiths-iMac.local>
2026-09-18 19:57:46 +0530

    Update API URLs to point to production backend on Render

--- admin_web_portal/lib/services/api_service.dart
+++ admin_web_portal/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://localhost:3000/api';
+  static const String baseUrl = 'https://protech-backend-83hl.onrender.com/api';
 
   Future<String?> _getToken() async {
     final prefs = await SharedPreferences.getInstance();
